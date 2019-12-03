0712556cc88eeaa1838629cc1e454343c1b886f1
hendri-tobing <tempa.staff@gmail.com>
2019-12-03 18:02:55 +0700

    update MqttApp

--- apps/mqtt_app/lib/mqtt_app.ex
+++ apps/mqtt_app/lib/mqtt_app.ex
@@ -1,18 +1,31 @@
 defmodule MqttApp do
-  @moduledoc """
-  Documentation for MqttApp.
-  """
 
-  @doc """
-  Hello world.
+  def start do
+    {:ok, _} =
+      Tortoise.Supervisor.start_child(
+        client_id: :a,
+        handler: {MqttApp.Handler, [name: :a]},
+        server: {Tortoise.Transport.Tcp, host: 'localhost', port: 1883},
+        subscriptions: [{"test/test1", 0}])
 
-  ## Examples
+    {:ok, _} =
+      Tortoise.Supervisor.start_child(
+        client_id: :b,
+        handler: {MqttApp.Handler, [name: :b]},
+        server: {Tortoise.Transport.Tcp, host: 'localhost', port: 1883},
+        subscriptions: [{"test/test2", 0}])
 
-      iex> MqttApp.hello()
-      :world
+    {:ok, _} =
+      Tortoise.Supervisor.start_child(
+        client_id: :aa,
+        handler: {MqttApp.Handler, [name: :b]},
+        server: {Tortoise.Transport.Tcp, host: 'localhost', port: 1883}
+      )
 
-  """
-  def hello do
-    :world
+  end
+
+  def publishmsg(client, tpc, msg) do
+    Tortoise.publish(client, tpc, msg)
   end
 end
+
